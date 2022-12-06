28c50e39ba77ffd9a1dc9248fedcb1a7f223a7f8
Harmeetrai <harmeetrai18@gmail.com>
2022-12-05 16:12:31 -0800

    update navigation links

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { images } from '../../constants'
+
 import './Navbar.scss'
 
 const Navbar = () => {
@@ -8,6 +10,14 @@ const Navbar = () => {
       <div>
         <img src={images.logo} alt="logo" />
       </div>
+      <ul>
+        {['Home', 'About', 'Skills', 'Work', 'Contact'].map((item, index) => (
+          <li key={`link-${item}`}>
+            <div/>
+            <a href={`#${item}`}>{item}</a>
+          </li>
+          ))}
+      </ul>
     </nav>
   )
 }
